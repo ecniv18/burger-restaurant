945efa5eee4204b52ad5ab827973501697b0843e
vince <167593601+ecniv18@users.noreply.github.com>
2024-09-25 16:49:03 +0800

    ADD: import  menu.js and displays it on button press

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import "./styles.css";
 
 import home from "./pages/home/home";
+import menu from "./pages/menu/menu";
 
 (function init() {
   const contentContainer = document.querySelector("#content");
   const homeButton = document.querySelector("#home-button");
+  const menuButton = document.querySelector("#menu-button");
 
   display(contentContainer, home());
 
-  [homeButton].forEach((button) => {
+  [homeButton, menuButton].forEach((button) => {
     button.addEventListener("click", () => {
       if (button.id === "home-button") {
         display(contentContainer, home());
+      } else if (button.id === "menu-button") {
+        display(contentContainer, menu());
       }
     });
   });
