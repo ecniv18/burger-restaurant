2fa6745433df8b89d2533089fd85f7f1eeb13b59
vince <167593601+ecniv18@users.noreply.github.com>
2024-09-27 12:17:34 +0800

    IMPORT: the about page file and displays it on button click

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import "./styles.css";
 
 import home from "./pages/home/home";
 import menu from "./pages/menu/menu";
+import about from "./pages/about/about";
 
 (function init() {
   const contentContainer = document.querySelector("#content");
   const homeButton = document.querySelector("#home-button");
   const menuButton = document.querySelector("#menu-button");
+  const aboutButton = document.querySelector("#about-button");
 
   display(contentContainer, home());
 
-  [homeButton, menuButton].forEach((button) => {
+  [homeButton, menuButton, aboutButton].forEach((button) => {
     button.addEventListener("click", () => {
       if (button.id === "home-button") {
         display(contentContainer, home());
       } else if (button.id === "menu-button") {
         display(contentContainer, menu());
+      } else if (button.id === "about-button") {
+        display(contentContainer, about());
       }
     });
   });
